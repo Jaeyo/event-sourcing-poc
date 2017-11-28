655b1425ca6aaf14008792e576a95d29dd16a31f
이재용 <lastiverse@gmail.com>
2017-11-28 18:59:12 +0900

    -

--- src/main/kotlin/org/jaeyo/eventsourcingpoc/common/config/H2Config.kt
+++ src/main/kotlin/org/jaeyo/eventsourcingpoc/common/config/H2Config.kt
@@ -1,10 +1,15 @@
 package org.jaeyo.eventsourcingpoc.common.config
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.servlet.Servlet
+
 //@Configuration
 class H2Config {
 //    @Bean
 //    fun h2ServletRegistration() =
-//        ServletRegistrationBean().apply {
+//        ServletRegistrationBean<Servlet>().apply {
 //            addUrlMappings("/console/*")
 //        }
 }
